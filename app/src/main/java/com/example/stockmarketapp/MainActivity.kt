4e55190a3a0d756b740d2e784b02065a4cb3a5bb
beslimir <marin.beslo@gmail.com>
2022-05-18 15:32:46 +0200

    Add navigation, color and theme

--- app/src/main/java/com/example/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/example/stockmarketapp/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.stockmarketapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.stockmarketapp.presentation.company_listings.NavGraphs
+import com.example.stockmarketapp.ui.theme.StockMarketAppTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.spec.NavGraphSpec
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +19,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            StockMarketAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.onBackground
+                ) {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
+                }
+            }
 
         }
     }

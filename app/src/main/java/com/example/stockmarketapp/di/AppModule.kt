d19c4a0e252ef4d894f35f2a9603e716f7edbbb4
beslimir <marin.beslo@gmail.com>
2022-05-18 14:59:16 +0200

    Implement Dagger Hilt

---
+++ app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.example.stockmarketapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.stockmarketapp.data.local.StockDatabase
+import com.example.stockmarketapp.data.remote.StockAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideStockApi(): StockAPI {
+        return Retrofit.Builder()
+            .baseUrl(StockAPI.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesStockDatabase(app: Application): StockDatabase {
+        return Room.databaseBuilder(
+            app,
+            StockDatabase::class.java,
+            "stockdb.db"
+        ).build()
+    }
+}
